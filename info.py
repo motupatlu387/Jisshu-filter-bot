0a36e6c1614908252d5fcedb95fe8d7579e3865a
motupatlu387 <motupatlu747474@gmail.com>
2025-03-18 10:53:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,20 +13,20 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '26963218'))
+API_HASH = environ.get('API_HASH', '4e552fd52643980359ae40e88b1a0d75')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8058944307:AAEdMeRI9LeX1Ki0Rk-JzU-hPdROPm7YxMY')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/JisshuMovieZone')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7080214888').split()]
+USERNAME = environ.get('USERNAME', "https://t.me/Coder839") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1002559564172'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+-C2AYMIX_WZlODI1')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1002545381881').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://motuji:motuji@motupatlu.cqsm6.mongodb.net/?retryWrites=true&w=majority&appName=Motupatlu")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Motupatlu")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
